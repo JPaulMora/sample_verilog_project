ef03f4a623f95a61ffce3ad920002a2dcee44af1
John Mora <juan@nimble.gt>
2024-12-01 19:19:31 -0600

    Update main_tb.v

--- main_tb.v
+++ main_tb.v
@@ -1,22 +1,29 @@
-module Not_tb;
-wire Y;
-reg A;
+//Test_Bench 
 
-Not compuerta(.A(A), .Y(Y));
+module nanochip_tb; 
+reg clk;
+reg reset;
+reg EN;
+reg [1:0] select;
+wire [7:0] q_out;
+wire clk_s;
 
-initial 
-begin
-	//$fsdbDumpfile("not.fsdb"); 
-	//$fsdbDumpvars(0, Not_tb);
-	$dumpfile("not.vcd");
-	$dumpvars(0, Not_tb);
-	A = 1'b0;
-	#1;
-	A = 1'b1;
-	#1;
-	A = 1'b0;
-	#1;
-	A = 1'b1;
-
+chip_SP chipl(q_out, reset, clk, EN, clk_s,select);
+initial
+clk = 1'b0;
+always
+#1 clk=~clk;
+initial begin
+$fsdbDumpfile("nanochip.fsdb"); 
+$fsdbDumpvars(0, nanochip_tb);
+$dumpfile("nanochip.vcd");
+$dumpvars(0, nanochip_tb);
+EN = 1'b0;
+reset = 1'b1;
+select = 2'b10;
+#15 reset =1'b0;
+#15 reset =1'b1; 
+#15 reset =1'b0;
+#8000 $finish;
 end
 endmodule
